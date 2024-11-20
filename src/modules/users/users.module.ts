b0a98b0cd842aa7af93203e4583b8a598334fda4
devkcng <kimmanhcuong230304@gmail.com>
2024-11-20 04:31:09 +0000

    fix: UsersService is not being properly injected into the AuthService

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from '@/modules/prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
